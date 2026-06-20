0778703c717cfce1a600323d15629f335640539c
SyahJehanWonopatih <syahjehan00@gmail.com>
2026-06-20 11:17:01 +0700

    form7 dan koneksi database

---
+++ Sistem pelaporan keracunan MBG/Form7.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistem_pelaporan_keracunan_MBG
+{
+    public partial class Form7 : Form
+    {
+        private readonly string connectionString =
+            "Data Source=TERABYTE\\SYAHJEHAN00;" +
+            "Initial Catalog=Sistem_Pelaporan_Keracunan_MBG;" +
+            "Integrated Security=True";
+
+        public Form7()
+        {
+            InitializeComponent();
+        }
+
+        private void Form7_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
