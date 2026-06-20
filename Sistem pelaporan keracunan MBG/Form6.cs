efdd2f257f9bddb46a5586218fc6cbed669a5748
SyahJehanWonopatih <syahjehan00@gmail.com>
2026-06-20 10:57:48 +0700

    menambah fungsi getlaporan

--- Sistem pelaporan keracunan MBG/Form6.cs
+++ Sistem pelaporan keracunan MBG/Form6.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -43,6 +44,41 @@ namespace Sistem_pelaporan_keracunan_MBG
                 MessageBox.Show("Gagal memuat laporan:\n" + ex.Message, "Error",
                     MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private List<LaporanData> GetDataLaporan()
+        {
+            List<LaporanData> list = new List<LaporanData>();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand("sp_ReportLaporan", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@tgl_dari", tglDari);
+                cmd.Parameters.AddWithValue("@tgl_sampai", tglSampai);
+
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new LaporanData
+                        {
+                            IdLaporan = Convert.ToInt32(reader["id_laporan"]),
+                            NamaPelapor = reader["nama_pelapor"].ToString(),
+                            Kontak = reader["kontak"].ToString(),
+                            KotaKab = reader["kota_kab"].ToString(),
+                            LokasiKejadian = reader["lokasi_kejadian"].ToString(),
+                            Tanggal = Convert.ToDateTime(reader["tanggal"]),
+                            JumlahKorban = Convert.ToInt32(reader["jumlah_korban"]),
+                            Gejala = reader["gejala"].ToString(),
+                            StatusValidasi = reader["status_validasi"].ToString()
+                        });
+                    }
+                }
+            }
+
+            return list;
         }
 
         private void crystalReportViewer1_Load(object sender, EventArgs e)
